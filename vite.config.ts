5f82923e5fea77c57a96b1d02c14a6dfd5579966
Appz-dk <patrick.fyhn@gmail.com>
2023-03-30 14:19:29 +0200

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
-/// <reference types="vite-plugin-svgr/client" />
 export default defineConfig({
   plugins: [react(), svgr()],
 })
